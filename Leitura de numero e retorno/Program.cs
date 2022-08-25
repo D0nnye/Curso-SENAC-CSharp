3131470c9704bd827a7cf000d4a1106bb44917b0
lucas.sleocadio <lucas.sleocadio@L130570776W10-1>
2022-08-25 17:22:13 -0300

    Att

---
+++ Leitura de numero e retorno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Leitura_de_numero_e_retorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //escrever um programa que leia um numero e retorne se ele é positivo ou negativo.
+
+            Console.WriteLine("Escreva um numero");
+            double num = double.Parse(Console.ReadLine());
+
+
+            if (num < 0)
+            {
+                Console.WriteLine($"O Numero {num} é negativo!");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine(" 0 é um número neutro");
+            }
+            else
+            {
+                Console.WriteLine($"O numero {num} é positivo");
+
+            }
+        }
+    }
+}
+
---
+++ Notas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+            Console.WriteLine("Escreva a primeira nota");
+            double N1= double.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva a segunda nota");
+            double N2= double.Parse(Console.ReadLine());
+
+
+            double M3 = N1 / N2;
+
+
+
+            if (M3<5)
+            {
+                Console.WriteLine($"Reprovado");
+            }
+            else if (M3==6)
+            {
+                Console.WriteLine("Média");
+            }
+            else
+            {
+                Console.WriteLine($"Aprovado");
+
+            }
+        }
+    }
+}
+       
+   
