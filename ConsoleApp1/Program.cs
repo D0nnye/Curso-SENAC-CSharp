5408c403c5025130f733828126abddb08b5b7b32
lucas.sleocadio <lucas.sleocadio@L130570776W10-1>
2022-08-25 16:02:55 -0300

    Subir calculo de m²

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, valorm2, area, valorTotal;
+
+            Console.WriteLine("Largura");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Comprimento");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor do M²");
+            valorm2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            valorTotal = area * valorm2;
+
+            Console.Clear();
+            Console.WriteLine("Resultado");
+            Console.WriteLine("Area do terreno é: " + area.ToString("F2", CultureInfo.InvariantCulture) + " m²");
+            Console.WriteLine("Valor total é R$: " + valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
