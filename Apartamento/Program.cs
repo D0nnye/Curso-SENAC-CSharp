017c8ce2623bda30bfff7393a8eb181fd4347024
lucas.sleocadio <lucas.sleocadio@L130570776W10-1>
2022-08-25 16:51:42 -0300

    Att

---
+++ Apartamento/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Apartamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura, comprimento, area;
+
+            Console.WriteLine("Largura do apartamento");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Comprimento do apartamento");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+
+            Console.Clear();
+
+            if (area <= 50)
+            {
+                Console.WriteLine("Apartamento compacto");
+                Console.WriteLine("Apartamento com area de " + area + "m²");
+
+            }
+            else
+            {
+                Console.WriteLine("Apartamento superluxo");
+                Console.WriteLine("Apartamento com area de " + area + "m²");
+
+
+
+            }
+        }
+    }
+}
+
+    
+
+   
---
+++ Estruturas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Estruturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Menor de idade");
+            }
+        }
+    }
+}
