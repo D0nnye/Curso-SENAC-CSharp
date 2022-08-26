081699f341a3a66569237870a67385959c13aada
lucas.sleocadio <lucas.sleocadio@L130570776W10-1>
2022-08-26 17:14:01 -0300

    Aula 26/08

---
+++ Anotacoes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Anotacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Com VARiavel dinamica
+            var nome = "MArcos";
+            var sobrenome = "Paulo";
+            Console.WriteLine(nome+sobrenome);
+
+            //Sem Variavel
+            string nome1 = "MArcos";
+            string sobrenome1 = "PAulo";
+            var nomecompleto = $"{nome} {sobrenome}";
+            Console.WriteLine(nomecompleto);
+
+            //Usar maiusculo
+            Console.WriteLine(nomecompleto.ToUpper());
+            //Minusculo
+            Console.WriteLine(nomecompleto.ToLower());
+            //Sublinhas com função contagem de caractere
+            Console.WriteLine(nomecompleto.Substring(6));
+            //Sub com função de mais caractere além da contagem
+            Console.WriteLine(nomecompleto.Substring(6, 6));
+            //Função para procurar se existe o argumento, ex se existe a leta M, para achar com mais de um caracteres só colocar em aspas duplas.
+            Console.WriteLine(nomecompleto.Contains('M'));
+            //Função que "começa com" a letra que você definir, para finalizar "com a letra" definida é EndsWith.
+            Console.WriteLine(nomecompleto.StartsWith("P"));
+            //Função para contar os caract até encontrar o argumento definido.
+            Console.WriteLine(nomecompleto.IndexOf("o"));
+            //Função para encontrar o ultimo argumento
+            Console.WriteLine(nomecompleto.LastIndexOf("o"));
+
+
+           
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Array/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int [] = Array, new int é você dizendo que o seu array é [3] tamanho três: 0,1,2.
+            /*
+            int[] numero = new int[3];
+            numero[0] = 10;
+            numero[1] = 11;
+            numero[2] = 12;
+
+            Console.WriteLine(numero[0]);
+            Console.WriteLine(numero[1]);
+            Console.WriteLine(numero[2]);
+            */
+
+            /*
+            // Imparares
+            // Pode deixar o codigo mais limpor adicionando os "impares[0]" do lado do new int[3] {1, 3, 5}.
+            int[] impares = new int[3];
+            impares[0] = 1;
+            impares[1] = 3;
+            impares[2] = 5;
+            Console.WriteLine(impares[0]);
+            Console.WriteLine(impares[1]);
+            Console.WriteLine(impares[2]);
+            */
+
+            //string[] nomes = new[] { "Mercedes", "Red Bull", "Aston Martin", "Ferrari", "Williams" };
+            //Console.WriteLine(nomes[0]);
+            //Console.WriteLine(nomes[1]);
+            //Console.WriteLine(nomes[2]);
+            //Console.WriteLine(nomes[3]);
+            //Console.WriteLine(nomes[4]);
+
+            //var a = 5;
+            //var b = 5;
+            //var c = 5;
+            //var d = 5.0;
+            //var e = 5;
+
+            //a = 1 + 2;
+            //b += 2; //incrementando
+            //c -= 2; //decrementando
+            //d /= 2; //divisao ** se o numero estiver inteiro vai dar 2 porem adicionando o 5.0 no "var d = 5.0" ele dividir corretamente.
+            //e %= 2; //modulo ** 
+
+
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+            //Console.WriteLine(e);
+
+            //var a = 2;
+            //var b = 3;
+            //Console.WriteLine(a == b); //Igual
+            //Console.WriteLine(a != b); //Diferent
+            //Console.WriteLine(a > b); // Maior
+            //Console.WriteLine(a < b); // Menor
+            //Console.WriteLine(a >= b); // Maior IGUAL
+            //Console.WriteLine(a <= b); // Menor IGUAL
+
+            var ativo = false;
+            var a = 3;
+            var b = 5;
+            Console.WriteLine(ativo == true);
+            Console.WriteLine(ativo == true);
+
+
+
+
+
+
+
+
+        }
+    }
+}
