b345b5d5fddd2965c7312d3f5c9c81b1ee705046
LUCAS DOS SANTOS LEOCADIO <lucas.sleocadio@senacsp.edu.br>
2022-08-26 01:52:53 -0300

    Att

--- Notas/Program.cs
+++ Notas/Program.cs
@@ -6,29 +6,40 @@ namespace Notas
     {
         static void Main(string[] args)
         {
-  
+
+            foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                Console.ForegroundColor = color;
+                Console.Clear();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Escreva a primeira nota");
             double N1= double.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Escreva a segunda nota");
             double N2= double.Parse(Console.ReadLine());
 
 
-            double M3 = N1 / N2;
+            double M3 = (N1 + N2) / 2;
+            
+            
 
-
-
-            if (M3<5)
+            if (M3>5)
             {
-                Console.WriteLine($"Reprovado");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Você foi aprovado com honrarias ao obter a nota {M3}.");
             }
-            else if (M3==6)
+            else if (M3==5)
             {
-                Console.WriteLine("Média");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Você foi aprovado dentro da média {M3}.");
             }
-            else
+            else 
             {
-                Console.WriteLine($"Aprovado");
-
+                Console.Clear();          
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ao atingir a média {M3}, você não atinge as exigências do curso e está reprovado.");
             }
         }
     }
--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -12,12 +12,12 @@ namespace Tabuada
             Console.Write("Digite um numero : ");
             numero = Int32.Parse(Console.ReadLine());
 
-            /*Com laço de repetição
+           
             for (contador = 1; contador <= 10; ++contador)
             {
                 formula = numero * contador;
                 Console.WriteLine(numero + " X " + contador + " = " + formula);
-            */
+           
             }
             }
     }
